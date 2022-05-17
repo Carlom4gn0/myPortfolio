f50dadf928289f829f884eb320ea6e9fe0688ada
Carlomagno José <66708949+Carlom4gn0@users.noreply.github.com>
2022-05-16 22:56:56 -0300

    Uploading files

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var typed = new Typed('.typed', {
+  strings: ['<i class="web-dev">Web Developer</i>',
+   '<i class="duin"> CMS in DUIN</i>'],
+
+   stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
+var typed = new Typed('.typed2', {
+    strings: ['<i class="web-dev2">Or follow me</i>',
+    '<i class="web-dev3">↓↓↓↓↓↓↓↓↓↓</i>',
+     ],
+  
+     stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+      typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+      startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+      backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+      smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+      shuffle: false, // Alterar el orden en el que escribe las palabras.
+      backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+      loop: true, // Repetir el array de strings
+      loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+      showCursor: true, // Mostrar cursor palpitanto
+      cursorChar: '|', // Caracter para el cursor
+      contentType: 'html', // 'html' o 'null' para texto sin formato
+  });
+  
